c2a3bebf224a086a39942248dcc9757d836966dd
gregnix <77677781+gregnix@users.noreply.github.com>
2024-04-07 08:47:01 +0200

    Create typedlist.tcl

---
+++ list/typedlist.tcl
@@ -0,0 +1,46 @@
+#! /usr/bin/env tclsh
+
+#20240407
+#typedList.tcl
+#inspired and partly adopted by
+#https://wiki.tcl-lang.org/page/typedlist
+#https://wiki.tcl-lang.org/page/Tcl%5FObj+proposals?R=0
+
+namespace eval type {
+    variable Usertype
+    proc set {*var type} {
+        variable Usertype
+        upvar ${*var} var
+        if {[info exist var]} {
+            dict set Usertype ${*var} $type
+        }
+    }
+    proc get {*var} {
+        variable Usertype
+        upvar ${*var} var
+        if {[info exist var]} {
+            dict get $Usertype ${*var}
+        }
+    }
+    namespace export *
+    namespace ensemble create
+}
+
+#Example
+if {[info script] eq $argv0 && 1 } {
+set A [list 100 100 200 200]
+type set A rectangle
+set B [list 100 100 200 200]
+type set B oval
+
+puts [type get A]
+puts [type get B]
+puts $A
+}
+
+#Output
+if {0} {
+rectangle
+oval
+100 100 200 200
+}
