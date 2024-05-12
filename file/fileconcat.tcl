2031d604653f300bb1b0b54295a4c9147014409c
gregnix <77677781+gregnix@users.noreply.github.com>
2024-05-12 22:01:17 +0200

    Update fileconcat.tcl

--- file/fileconcat.tcl
+++ file/fileconcat.tcl
@@ -25,27 +25,37 @@ puts "   [info patchlevel] $tcl_platform(os)"
 puts " absolute"
 set ndir [pwd]
 puts "ndir: $ndir"
+puts "file normalize: [file normalize $ndir]"
 puts "file split: [file split [file nativename $ndir]]"
 puts "file concat  [file concat /tmp $ndir]"
 
 puts "\n  relative"
 set ndir ./greg/tmp
 puts "ndir: $ndir"
+puts "file normalize: [file normalize $ndir]"
 puts "file split: [file split [file nativename $ndir]]"
-puts "file concat:  [file concat /tmp $ndir]]"
+puts "file concat:  [file concat /tmp $ndir]"
 
 puts "\n unc path in windows"
 set ndir {\\myserver\files\projects\tcl\info.txt}
 puts "ndir: $ndir"
+puts "file normalize: [file normalize $ndir]"
 puts "file split: [file split [file nativename $ndir]]"
 puts "file concat:  [file concat /tmp $ndir]"
 
 puts "\n unc path in windows"
 set ndir //myserver/files/projects/tcl/info.txt
 puts "ndir: $ndir"
+puts "file normalize: [file normalize $ndir]"
 puts "file split: [file split [file nativename $ndir]]"
 puts "file concat:  [file concat /tmp $ndir]"
 
+puts "\n unc path in windows"
+set ndir //myserver/files/projects/tcl/info.txt
+puts "ndir: $ndir"
+puts "file normalize: [file normalize $ndir]"
+puts "file split: [file split [file nativename $ndir]]"
+puts "file concat:  [file concat //tmpserver/tmp $ndir]"
 
 
 #This Tcl script defines a procedure 
@@ -58,45 +68,65 @@ if {0} {
    8.6.13 Windows NT
  absolute
 ndir: F:/tcltk/project/file
+file normalize: F:/tcltk/project/file
 file split: F:/ tcltk project file
 file concat  F:/tmp/tcltk/project/file
 
   relative
 ndir: ./greg/tmp
+file normalize: F:/tcltk/project/file/greg/tmp
 file split: . greg tmp
-file concat:  F:/tmp/greg/tmp]
+file concat:  F:/tmp/greg/tmp
 
  unc path in windows
 ndir: \\myserver\files\projects\tcl\info.txt
+file normalize: //myserver/files/projects/tcl/info.txt
 file split: //myserver/files projects tcl info.txt
 file concat:  F:/tmp/projects/tcl/info.txt
 
  unc path in windows
 ndir: //myserver/files/projects/tcl/info.txt
+file normalize: //myserver/files/projects/tcl/info.txt
 file split: //myserver/files projects tcl info.txt
 file concat:  F:/tmp/projects/tcl/info.txt
+
+ unc path in windows
+ndir: //myserver/files/projects/tcl/info.txt
+file normalize: //myserver/files/projects/tcl/info.txt
+file split: //myserver/files projects tcl info.txt
+file concat:  //tmpserver/tmp/projects/tcl/info.txt
 
   
   Linux Output:
    8.6.14 Linux
  absolute
 ndir: /media/lnet/tcltk/project/file
+file normalize: /media/lnet/tcltk/project/file
 file split: / media lnet tcltk project file
 file concat  /tmp/media/lnet/tcltk/project/file
 
   relative
 ndir: ./greg/tmp
+file normalize: /media/lnet/tcltk/project/file/greg/tmp
 file split: . greg tmp
-file concat:  /tmp/greg/tmp]
+file concat:  /tmp/greg/tmp
 
  unc path in windows
 ndir: \\myserver\files\projects\tcl\info.txt
+file normalize: /media/lnet/tcltk/project/file/\\myserver\files\projects\tcl\info.txt
 file split: {\\myserver\files\projects\tcl\info.txt}
 file concat:  /tmp
 
  unc path in windows
 ndir: //myserver/files/projects/tcl/info.txt
+file normalize: /myserver/files/projects/tcl/info.txt
 file split: / myserver files projects tcl info.txt
 file concat:  /tmp/myserver/files/projects/tcl/info.txt
+
+ unc path in windows
+ndir: //myserver/files/projects/tcl/info.txt
+file normalize: /myserver/files/projects/tcl/info.txt
+file split: / myserver files projects tcl info.txt
+file concat:  /tmpserver/tmp/myserver/files/projects/tcl/info.txt
 
 }
