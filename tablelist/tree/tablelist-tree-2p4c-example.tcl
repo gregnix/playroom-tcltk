df3c88b4aa4a62e464b25e3a26cd265e2b6c1884
gregnix <77677781+gregnix@users.noreply.github.com>
2024-06-09 15:05:09 +0200

    Add files via upload

--- tablelist/tree/tablelist-tree-2p4c-example.tcl
+++ tablelist/tree/tablelist-tree-2p4c-example.tcl
@@ -2,11 +2,12 @@
 
 #20240609
 
-# e
+# 
 package require Tk
 package require tablelist_tile
 
 package require struct::list
+# help procs
 # This function generates flat hierarchical test data
 proc generateTreeData1 {{dataList {1 2 3}} {parentList {a b c}} {shuffle 1} } {
     set length [llength $dataList]
@@ -61,111 +62,16 @@ proc treetblcreate {w} {
     pack $frb -expand no -fill x
 
     set bodyTag [$tbl bodytag]
-    #bind $bodyTag <Double-1>   [list callbDouble1 %W %x %y]
     bind $bodyTag <Double-1>  [list callbOutput $tbl]
     return $tbl
 }
 
-# for debug
-proc callbOutput {tbl} {
-    variable data
-    set row k0
-    set rowa [$tbl index active]
-    set rows [$tbl curselection]
-    set curcellselection [$tbl curcellselection]
-    lappend  parentsRoot root [$tbl childkeys root]
-    set parentkey [$tbl parentkey $row]
-    set childcount [$tbl childcount $row]
-    set childindex [$tbl childindex $row]
-    set descendantcount [$tbl  descendantcount $row]
-    set childkeys  [$tbl childkeys $row]
-    set depth [$tbl depth $row]
-    set childcountpk [$tbl childcount $parentkey]
-
-    if {$parentkey eq "root"} {
-        set childindexpk [$tbl childindex [lindex [$tbl childkeys root] 0]]
-    } else {
-        set childindexpk [$tbl childindex $parentkey]
-    }
-
-    set childkeyspk  [$tbl childkeys $parentkey]
-    set depthpk [$tbl depth $parentkey]
-
-    set parentkeya [$tbl parentkey $rowa]
-    set childcounta [$tbl childcount $rowa]
-    set childindexa [$tbl childindex $rowa]
-    set descendantcounta [$tbl  descendantcount $rowa]
-    set childkeysa  [$tbl childkeys $rowa]
-    set deptha [$tbl depth $rowa]
-
-    set noderow [$tbl noderow $parentkey $childindex]
-    set childKindex [lindex $childkeys $childindex]
-    set toplevelkey [$tbl toplevelkey $row]
-    # Output widget
-    set top .topContent
-    set f $top.ft
-    set t $f.t
-    if {![winfo exists $top]} {
-        toplevel $top
-        frame $f
-        pack $f -side top -fill both -expand true
-        set t [text $f.t -setgrid true -wrap none -width 120 \
-    -yscrollcommand "$f.vset set" -xscrollcommand "$f.hset set"]
-        scrollbar $f.vset -orient vert -command "$f.t yview"
-        scrollbar $f.hset -orient hori -command "$f.t xview"
-        pack $f.hset -side bottom -fill x
-        pack $f.vset -side right -fill y
-        pack $f.t -side left -fill both -expand true
-        wm geometry $top +0+0
-    }
-    $t delete 1.0 end
-    $t insert end  "\n set row 0\n"
-    $t insert end   "row: $row :: rows : $rows :: noderow: $noderow :: [$tbl getkey $row]\n"
-    $t insert end   "parentsRoot: $parentsRoot ::  parentkey: $parentkey ::  descendantcount: $descendantcount\n"
-    $t insert end   "childcountpk : $childcountpk :: childindexpk: $childindexpk :: childkeyspk: $childkeyspk :: depthpk : $depthpk\n"
-    $t insert end   "childcount : $childcount :: childindex: $childindex :: childkeys: $childkeys :: depth : $depth\n"
-    $t insert end   "active rowa: $rowa :: \$tbl getkey \$rowa: [$tbl getkey $rowa]:: [$tbl getfullkey $rowa $rowa]\n"
-    $t insert end   "active parentkey: $parentkeya ::  descendantcount: $descendantcounta\n"
-    $t insert end   "active childcount : $childcounta :: childindex: $childindexa :: childkeys: $childkeysa :: depth : $deptha\n"
-    $t insert end  "\n \$tbl get 0 end\n"
-    $t insert end  [join [$tbl get 0 end] \n]
-    $t insert end  "\n\n  dumptostring"
-    $t insert end [$tbl dumptostring]
-    $t insert end  "\n\n  data  length [llength $data]\n"
-    $t insert end  [join $data \n]
-}
-
-#set bodyTag [$tbl bodytag]
-#bind $bodyTag <Double-1>   [list callbDouble1 %W %x %y]
-proc callbDouble1 {W x y } {
-    foreach {tbl x y} [tablelist::convEventFields $W $x $y] {}
-    set row [$tbl containing  $y]
-    set rows [$tbl curselection]
-    set curcellselection [$tbl curcellselection]
-    lappend  parentsRoot root [$tbl childkeys root]
-    set parentkey [$tbl parentkey $row]
-    set childcount [$tbl childcount $row]
-    set childindex [$tbl childindex $row]
-    set descendantcount [$tbl  descendantcount $row]
-    set childkeys  [$tbl childkeys $row]
-    set depth [$tbl depth $row]
-    set childkeyspk  [$tbl childkeys $parentkey]
-    set depthpk [$tbl depth $parentkey]
-    set noderow [$tbl noderow $parentkey $childindex]
-    set childKindex [lindex $childkeys $childindex]
-    set toplevelkey [$tbl toplevelkey $row]
-
-    puts \n
-    puts "row: $row :: rows : $rows :: noderow: $noderow :: [$tbl getkey $row]"
-    puts "parentsRoot: $parentsRoot ::  parentkey: $parentkey ::  descendantcount: $descendantcount"
-    puts "childcount : $childcount :: childindex: $childindex :: childkeys: $childkeys :: depth : $depth"
-}
 
 # Event-Handler 
 proc expandNode {tbl row} {
     # Hier kann Code hinzugefügt werden, um zusätzliche Daten dynamisch zu laden
     puts "Expanding node at $row"
-    contentNode $tbl $row
+    #contentNode $tbl $row
 }
 
 proc collapseNode {tbl row} {
@@ -205,27 +111,85 @@ proc populateTree {tbl} {
     callbOutput $tbl
 }
 
-# not in use
-proc contentNode {tbl row} {
-    # Überprüfen, ob Kinder bereits geladen wurden
-    return
-    if {[$tbl childcount $row] == 0} {
-        # Beispiel: Lade Daten basierend auf der ID des Knotens
-        set id [$tbl cellcget $row,0 -text]
-        set data [testData 5]  ;# Generiert 5 Testdatensätze
-        foreach item $data {
-            set id [lindex $item 0]
-            set alpha [lindex $item 1]
-            set num [lindex $item 2]
-            set date [lindex $item 3]
-            puts " $id $alpha $num $date"
-            $tbl insertchild $row end [list $id $alpha $num $date]
-        }
-
+# for debug
+proc callbOutput {tbl} {
+    variable data
+    set row [lindex [$tbl childkeys root] 0]
+    set rows [$tbl curselection]
+    #set curcellselection [$tbl curcellselection]
+    lappend  parentsRoot root [$tbl childkeys root]
+    set parentkey [$tbl parentkey $row]
+    set childcount [$tbl childcount $row]
+    set childindex [$tbl childindex $row]
+    set descendantcount [$tbl  descendantcount $row]
+    set childkeys  [$tbl childkeys $row]
+    set depth [$tbl depth $row]
+    set noderow [$tbl noderow $parentkey $childindex]
+    set childKindex [lindex $childkeys $childindex]
+    set toplevelkey [$tbl toplevelkey $row]
+    
+    #pk
+    set childcountpk [$tbl childcount $parentkey]
+    if {$parentkey eq "root"} {
+        set childindexpk [$tbl childindex [lindex [$tbl childkeys root] 0]]
+    } else {
+        set childindexpk [$tbl childindex $parentkey]
     }
+    set childkeyspk  [$tbl childkeys $parentkey]
+    set depthpk [$tbl depth $parentkey]
+    set descendantcountpk [$tbl  descendantcount $parentkey]
+    
+    #active rowa
+    set rowa [$tbl index active]
+    set parentkeya [$tbl parentkey $rowa]
+    set childcounta [$tbl childcount $rowa]
+    set childindexa [$tbl childindex $rowa]
+    set descendantcounta [$tbl  descendantcount $rowa]
+    set childkeysa  [$tbl childkeys $rowa]
+    set deptha [$tbl depth $rowa]
+
+    # Output widget
+    set top .topContent
+    set f $top.ft
+    set t $f.t
+    if {![winfo exists $top]} {
+        toplevel $top
+        frame $f
+        pack $f -side top -fill both -expand true
+        set t [text $f.t -setgrid true -wrap none -width 120 \
+    -yscrollcommand "$f.vset set" -xscrollcommand "$f.hset set"]
+        scrollbar $f.vset -orient vert -command "$f.t yview"
+        scrollbar $f.hset -orient hori -command "$f.t xview"
+        pack $f.hset -side bottom -fill x
+        pack $f.vset -side right -fill y
+        pack $f.t -side left -fill both -expand true
+        wm geometry $top +0+0
+    }
+    $t delete 1.0 end
+    $t insert end   "row: $row :: rows : $rows :: noderow: $noderow :: [$tbl getkey $row]\n"
+    $t insert end   "parentsRoot: $parentsRoot ::  parentkey: $parentkey ::  descendantcount: $descendantcountpk\n"
+    $t insert end   "childcount : $childcount :: childindex: $childindex :: childkeys: $childkeyspk :: depth : $depthpk\n"
+    $t insert end   "\n"
+    $t insert end   "row: $row :: \$tbl getkey \$row: [$tbl getkey $row]:: [$tbl getfullkey $row $row]\n"
+    $t insert end   "parentkey: $parentkey ::  descendantcount: $descendantcount\n"
+    $t insert end   "childcount : $childcount :: childindex: $childindex :: childkeys: $childkeys :: depth : $depth\n"
+    $t insert end   "\n"
+    $t insert end   "active rowa: $rowa :: \$tbl getkey \$rowa: [$tbl getkey $rowa]:: [$tbl getfullkey $rowa $rowa]\n"
+    $t insert end   "active parentkey: $parentkeya ::  descendantcount: $descendantcounta\n"
+    $t insert end   "active childcount : $childcounta :: childindex: $childindexa :: childkeys: $childkeysa :: depth : $deptha\n"
+    $t insert end  "\n \$tbl get 0 end\n"
+    $t insert end  [join [$tbl get 0 end] \n]
+    $t insert end  "\n\n  dumptostring"
+    $t insert end [$tbl dumptostring]
+    $t insert end  "\n\n  data  length [llength $data]\n"
+    $t insert end  [join $data \n]
 }
 
-# main and gui
+
+
+#########################
+# main 
+# gui
 wm title . "Tablelist Tree example with flate test data"
 set mainFrame [ttk::frame .main]
 pack $mainFrame -expand yes -fill both
@@ -233,6 +197,7 @@ pack $mainFrame -expand yes -fill both
 set tbl [treetblcreate $mainFrame]
 populateTree $tbl
 
+#########################
 if {0} {
 Output:
 
