3fc3da8ae6f6ce8240fc3a8c879a9af55f2e94cf
gregnix <77677781+gregnix@users.noreply.github.com>
2024-06-10 17:48:12 +0200

    Add files via upload

--- tablelist/tree/tablelist-tree-3p4c-example.tcl
+++ tablelist/tree/tablelist-tree-3p4c-example.tcl
@@ -9,7 +9,7 @@ package require tablelist_tile
 package require struct::list
 # help procs
 # This function generates flat hierarchical test data
-proc generateTreeData {{dataList {1 2 3}} {parentList {a b c}} {shuffle 1} } {
+proc generateSimpleLists {{dataList {1 2 3}} {parentList {a b c}} {shuffle 1} } {
     set length [llength $dataList]
     puts $length
     set dataList [::struct::list permutations $dataList]
@@ -37,12 +37,6 @@ proc generateLists {{parents {a b}}  {oneList {1 2 }} {twoList {1 2 }} {threeLis
     set twoList [::struct::list permutations $twoList]
     set threeList [::struct::list permutations $threeList]
     set fourList [::struct::list permutations $fourList]
-    if {$simple} {
-        set oneList [simple $oneList 0 1]
-        set twoList [simple $twoList 0 2]
-        set threeList [simple $threeList 0 3]
-        set fourList [simple $fourList 0 4]
-    }
     foreach parent $parents {
         foreach one $oneList {
             if {$flat} {set one [string map {{ } {}} $one]}
@@ -71,18 +65,7 @@ proc generateLists {{parents {a b}}  {oneList {1 2 }} {twoList {1 2 }} {threeLis
     }
     return $resultList
 }
-proc simple {liste pos wert} {
-    set newliste {}
-    if {[llength $liste] <= 1} {
-        return "{}"
-    }
-    foreach sublist $liste {
-        if {[lindex $sublist $pos] == $wert} {
-            lappend newliste $sublist
-        }
-    }
-    return $newliste
-}
+
 
 proc treetblcreate {w} {
     set frt [ttk::frame $w.frt]
@@ -98,7 +81,7 @@ proc treetblcreate {w} {
 
     # Enable expansion and collapse of nodes, sorting
     $frt.tbl configure -expandcommand "expandNode" -collapsecommand "collapseNode" \
-    -labelcommand tablelist::sortByColumn
+    -labelcommand tablelist::sortByColumn 
 
     # Scrollbars
     set vsb [scrollbar $frt.vsb -orient vertical -command [list $tbl yview]]
@@ -111,13 +94,16 @@ proc treetblcreate {w} {
     pack $frt -expand yes -fill both
 
     set frb [ttk::frame $w.frb]
-    set btnOutput  [ttk::button $frt.btnOutput -text Output -command [list callbOutput $tbl]]
-    set btnPopulate  [ttk::button $frt.btnPopulate -text "Populate" -command [list populateTree $tbl]]
-    pack $btnOutput $btnPopulate -side left
+    set cbGenerator [ttk::combobox $frb.cbGenerator -values [list generateSimpleLists generateLists]]
+    $cbGenerator current 0
+    set btnOutput  [ttk::button $frb.btnOutput -text Output -command [list callbOutput $tbl]]
+    set btnPopulate  [ttk::button $frb.btnPopulate -text "Populate" -command [list populateTree $tbl $cbGenerator ]]
+    pack $btnOutput $cbGenerator  $btnPopulate -side left
     pack $frb -expand no -fill x
 
     set bodyTag [$tbl bodytag]
     bind $bodyTag <Double-1>  [list callbOutput $tbl]
+    populateTree $tbl $cbGenerator
     return $tbl
 }
 
@@ -134,12 +120,14 @@ proc collapseNode {tbl row} {
 }
 
 # Function  Init
-proc populateTree {tbl} {
+proc populateTree {tbl cbGenerator} {
+    puts "cbGenerator: $cbGenerator"
     variable data
     $tbl delete 0 end
-    set data [generateTreeData {1 2 3 } {a b c} 0]
-    set data [generateLists {a b c} {1 2 3} {1 2 3} {1 2 3 } {} 1 1 1]
+    #set data [generateSimpleLists {1 2 3 } {a b c} 0]
+    #set data [generateLists {a b c} {1 2 3} {1 2 3} {1 2 3 } {} 1 1 1]
     #set data [generateLists ]
+    set data [[$cbGenerator get]]
     set idliste [list]
     foreach item $data {
         set id [lindex $item 0]
@@ -150,31 +138,42 @@ proc populateTree {tbl} {
         if {$id ni $idliste} {
             lappend idliste $id
             set idliste [lsort -unique $idliste]
-            set rootid [$tbl insertchild root end [list $id]]
+            set currentid [$tbl insertchild root end [list $id]]
+            set currentid [$tbl insertchild $currentid end [list $id $one]]
+            set currentid [$tbl insertchild $currentid end [list $id $one $two]]
         } else {
             foreach idrow [$tbl childkeys root] {
                 if {[lindex [$tbl rowcget $idrow -text] 0]  eq $id } {
-                    set rootid $idrow
+                    set currentid $idrow
                     foreach onerow [$tbl childkeys $idrow] {
                         if {[lindex [$tbl rowcget $onerow -text] 1]  eq $one } {
-                            set rootid $onerow
+                            set currentid $onerow
                             foreach tworow [$tbl childkeys $onerow] {
                                 if {[lindex [$tbl rowcget $tworow -text] 2]  eq $two } {
-                                    set rootid $tworow
+                                    set currentid $tworow
                                 }
                             }
+                            if {$currentid eq $onerow } {
+                                set currentid [$tbl insertchild $currentid end [list $id $one $two]]
+                            }
                         }
+                    }
+                    if {$currentid eq $idrow } {
+                        set currentid [$tbl insertchild $currentid end [list $id $one]]
                     }
                 }
             }
         }
-        $tbl insertchild $rootid end [list $id $one $two $three $four]
+        $tbl insertchild $currentid end [list $id $one $two $three $four]
     }
-    $tbl resetsortinfo
-    #$tbl refreshsorting 3
-    #$tbl refreshsorting 2
-    #$tbl refreshsorting 1
-    $tbl refreshsorting 0
+    #$tbl sort -increasing
+    #$tbl columnconfigure $col -sortmode integer
+    $tbl sortbycolumn 4 -increasing
+    $tbl sortbycolumn 3 -increasing
+    $tbl sortbycolumn 2 -increasing
+    $tbl sortbycolumn 1 -increasing
+    $tbl sortbycolumn 0 -increasing
+    #$tbl refreshsorting root
 
     callbOutput $tbl
 }
@@ -258,230 +257,98 @@ proc callbOutput {tbl} {
 #########################
 # main
 # gui
+## tbl init
+
+
 wm title . "Tablelist Tree example with flate test data"
 set mainFrame [ttk::frame .main]
 pack $mainFrame -expand yes -fill both
 
 set tbl [treetblcreate $mainFrame]
-populateTree $tbl
 
 #########################
 if {0} {
+Output:
+row: k6 :: rows : 11 :: noderow: 0 :: 6
+parentsRoot: root {k6 k12 k0} ::  parentkey: root ::  descendantcount: 42
+childcount : 3 :: childindex: 0 :: childkeys: k6 k12 k0 :: depth : 0
 
-    Output:
-    row: k0 :: rows : 26 :: noderow: 0 :: 0
-    parentsRoot: root {k0 k2} ::  parentkey: root ::  descendantcount: 98
-    childcount : 2 :: childindex: 0 :: childkeys: k0 k2 :: depth : 0
+row: k6 :: $tbl getkey $row: 6:: k6
+parentkey: root ::  descendantcount: 13
+childcount : 3 :: childindex: 0 :: childkeys: k7 k10 k34 :: depth : 1
 
-    row: k0 :: $tbl getkey $row: 0:: k0
-    parentkey: root ::  descendantcount: 48
-    childcount : 2 :: childindex: 0 :: childkeys: k1 k8 :: depth : 1
+active rowa: 11 :: $tbl getkey $rowa: 36:: k36
+active parentkey: k34 ::  descendantcount: 1
+active childcount : 1 :: childindex: 0 :: childkeys: k37 :: depth : 3
 
-    active rowa: 26 :: $tbl getkey $rowa: 13:: k13
-    active parentkey: k8 ::  descendantcount: 11
-    active childcount : 11 :: childindex: 0 :: childkeys: k24 k33 k34 k43 k59 k77 k84 k85 k86 k90 k94 :: depth : 3
+ $tbl get 0 end
+a {} {} {} {}
+a 001 {} {} {}
+a 001 012 {} {}
+a 001 012 023 {}
+a 001 013 {} {}
+a 001 013 022 {}
+a 002 {} {} {}
+a 002 011 {} {}
+a 002 011 023 {}
+a 002 013 021 {}
+a 003 {} {} {}
+a 003 011 {} {}
+a 003 011 022 {}
+a 003 012 021 {}
+b {} {} {} {}
+b 101 {} {} {}
+b 101 112 {} {}
+b 101 112 123 {}
+b 101 113 {} {}
+b 101 113 122 {}
+b 102 {} {} {}
+b 102 111 {} {}
+b 102 111 123 {}
+b 102 113 121 {}
+b 103 {} {} {}
+b 103 111 {} {}
+b 103 111 122 {}
+b 103 112 121 {}
+c {} {} {} {}
+c 201 {} {} {}
+c 201 212 223 {}
+c 201 213 {} {}
+c 201 213 222 {}
+c 202 {} {} {}
+c 202 211 223 {}
+c 202 213 {} {}
+c 202 213 221 {}
+c 203 {} {} {}
+c 203 211 {} {}
+c 203 211 222 {}
+c 203 212 {} {}
+c 203 212 221 {}
 
-    $tbl get 0 end
-    a {} {} {} {}
-    a {2 1} {1 2} {2 1} {2 3 1}
-    a {2 1} {2 1} {2 1} {2 3 1}
-    a {2 1} {2 1} {1 2} {3 1 2}
-    a {2 1} {2 1} {2 1} {3 1 2}
-    a {2 1} {2 1} {2 1} {2 1 3}
-    a {2 1} {2 1} {1 2} {1 3 2}
-    a {2 1} {2 1} {1 2} {2 1 3}
-    a {2 1} {2 1} {1 2} {1 2 3}
-    a {2 1} {2 1} {1 2} {2 3 1}
-    a {2 1} {2 1} {1 2} {3 2 1}
-    a {2 1} {2 1} {2 1} {1 2 3}
-    a {2 1} {2 1} {2 1} {1 3 2}
-    a {2 1} {2 1} {2 1} {3 2 1}
-    a {2 1} {1 2} {2 1} {1 2 3}
-    a {2 1} {1 2} {1 2} {3 2 1}
-    a {2 1} {1 2} {1 2} {3 1 2}
-    a {2 1} {1 2} {1 2} {1 2 3}
-    a {2 1} {1 2} {2 1} {1 3 2}
-    a {2 1} {1 2} {1 2} {1 3 2}
-    a {2 1} {1 2} {1 2} {2 1 3}
-    a {2 1} {1 2} {2 1} {3 1 2}
-    a {2 1} {1 2} {2 1} {2 1 3}
-    a {2 1} {1 2} {1 2} {2 3 1}
-    a {2 1} {1 2} {2 1} {3 2 1}
-    a {1 2} {2 1} {1 2} {2 3 1}
-    a {1 2} {1 2} {1 2} {2 3 1}
-    a {1 2} {1 2} {1 2} {3 1 2}
-    a {1 2} {1 2} {2 1} {1 3 2}
-    a {1 2} {1 2} {1 2} {1 3 2}
-    a {1 2} {1 2} {2 1} {2 1 3}
-    a {1 2} {1 2} {2 1} {1 2 3}
-    a {1 2} {1 2} {2 1} {3 1 2}
-    a {1 2} {1 2} {2 1} {3 2 1}
-    a {1 2} {1 2} {1 2} {3 2 1}
-    a {1 2} {1 2} {2 1} {2 3 1}
-    a {1 2} {1 2} {1 2} {1 2 3}
-    a {1 2} {1 2} {1 2} {2 1 3}
-    a {1 2} {2 1} {2 1} {3 2 1}
-    a {1 2} {2 1} {2 1} {1 3 2}
-    a {1 2} {2 1} {2 1} {2 3 1}
-    a {1 2} {2 1} {1 2} {3 2 1}
-    a {1 2} {2 1} {1 2} {2 1 3}
-    a {1 2} {2 1} {2 1} {2 1 3}
-    a {1 2} {2 1} {1 2} {1 3 2}
-    a {1 2} {2 1} {2 1} {1 2 3}
-    a {1 2} {2 1} {2 1} {3 1 2}
-    a {1 2} {2 1} {1 2} {1 2 3}
-    a {1 2} {2 1} {1 2} {3 1 2}
-    b {} {} {} {}
-    b {1 2} {1 2} {2 1} {3 1 2}
-    b {1 2} {2 1} {1 2} {3 2 1}
-    b {1 2} {2 1} {2 1} {2 1 3}
-    b {1 2} {2 1} {2 1} {2 3 1}
-    b {1 2} {2 1} {2 1} {1 3 2}
-    b {1 2} {2 1} {2 1} {3 2 1}
-    b {1 2} {2 1} {1 2} {1 3 2}
-    b {1 2} {2 1} {2 1} {3 1 2}
-    b {1 2} {2 1} {1 2} {3 1 2}
-    b {1 2} {2 1} {1 2} {2 1 3}
-    b {1 2} {2 1} {1 2} {1 2 3}
-    b {1 2} {2 1} {1 2} {2 3 1}
-    b {1 2} {2 1} {2 1} {1 2 3}
-    b {1 2} {1 2} {2 1} {1 2 3}
-    b {1 2} {1 2} {2 1} {3 2 1}
-    b {1 2} {1 2} {1 2} {1 3 2}
-    b {1 2} {1 2} {1 2} {2 1 3}
-    b {1 2} {1 2} {1 2} {1 2 3}
-    b {1 2} {1 2} {2 1} {2 3 1}
-    b {1 2} {1 2} {1 2} {3 2 1}
-    b {1 2} {1 2} {1 2} {3 1 2}
-    b {1 2} {1 2} {2 1} {2 1 3}
-    b {1 2} {1 2} {1 2} {2 3 1}
-    b {1 2} {1 2} {2 1} {1 3 2}
-    b {2 1} {1 2} {1 2} {2 3 1}
-    b {2 1} {2 1} {2 1} {3 2 1}
-    b {2 1} {2 1} {1 2} {1 3 2}
-    b {2 1} {2 1} {2 1} {3 1 2}
-    b {2 1} {2 1} {1 2} {2 3 1}
-    b {2 1} {2 1} {2 1} {2 1 3}
-    b {2 1} {2 1} {1 2} {3 2 1}
-    b {2 1} {2 1} {2 1} {1 2 3}
-    b {2 1} {2 1} {2 1} {1 3 2}
-    b {2 1} {2 1} {2 1} {2 3 1}
-    b {2 1} {2 1} {1 2} {3 1 2}
-    b {2 1} {2 1} {1 2} {2 1 3}
-    b {2 1} {2 1} {1 2} {1 2 3}
-    b {2 1} {1 2} {2 1} {1 2 3}
-    b {2 1} {1 2} {1 2} {1 3 2}
-    b {2 1} {1 2} {1 2} {2 1 3}
-    b {2 1} {1 2} {2 1} {3 2 1}
-    b {2 1} {1 2} {2 1} {1 3 2}
-    b {2 1} {1 2} {1 2} {1 2 3}
-    b {2 1} {1 2} {1 2} {3 2 1}
-    b {2 1} {1 2} {1 2} {3 1 2}
-    b {2 1} {1 2} {2 1} {2 1 3}
-    b {2 1} {1 2} {2 1} {2 3 1}
-    b {2 1} {1 2} {2 1} {3 1 2}
+  dumptostringid one two three four
+0 increasing
 
-    dumptostringid one two three four
-    {} {}
+-1 0 1 2 1 4 0 6 7 6 0 10 11 10 -1 14 15 16 15 18 14 20 21 20 14 24 25 24 -1 28 29 29 31 28 33 33 35 28 37 38 37 40
+{a {} {} {} {}} {a 001 {} {} {}} {a 001 012 {} {}} {a 001 012 023 {}} {a 001 013 {} {}} {a 001 013 022 {}} {a 002 {} {} {}} {a 002 011 {} {}} {a 002 011 023 {}} {a 002 013 021 {}} {a 003 {} {} {}} {a 003 011 {} {}} {a 003 011 022 {}} {a 003 012 021 {}} {b {} {} {} {}} {b 101 {} {} {}} {b 101 112 {} {}} {b 101 112 123 {}} {b 101 113 {} {}} {b 101 113 122 {}} {b 102 {} {} {}} {b 102 111 {} {}} {b 102 111 123 {}} {b 102 113 121 {}} {b 103 {} {} {}} {b 103 111 {} {}} {b 103 111 122 {}} {b 103 112 121 {}} {c {} {} {} {}} {c 201 {} {} {}} {c 201 212 223 {}} {c 201 213 {} {}} {c 201 213 222 {}} {c 202 {} {} {}} {c 202 211 223 {}} {c 202 213 {} {}} {c 202 213 221 {}} {c 203 {} {} {}} {c 203 211 {} {}} {c 203 211 222 {}} {c 203 212 {} {}} {c 203 212 221 {}}
 
-    -1 0 1 2 2 2 2 2 2 2 2 2 2 2 1 14 14 14 14 14 14 14 14 14 14 0 25 26 26 26 26 26 26 26 26 26 26 26 25 38 38 38 38 38 38 38 38 38 38 -1 49 50 51 51 51 51 51 51 51 51 51 51 51 50 63 63 63 63 63 63 63 63 63 63 49 74 75 75 75 75 75 75 75 75 75 75 75 74 87 87 87 87 87 87 87 87 87 87
-    {a {} {} {} {}} {a {2 1} {1 2} {2 1} {2 3 1}} {a {2 1} {2 1} {2 1} {2 3 1}} {a {2 1} {2 1} {1 2} {3 1 2}} {a {2 1} {2 1} {2 1} {3 1 2}} {a {2 1} {2 1} {2 1} {2 1 3}} {a {2 1} {2 1} {1 2} {1 3 2}} {a {2 1} {2 1} {1 2} {2 1 3}} {a {2 1} {2 1} {1 2} {1 2 3}} {a {2 1} {2 1} {1 2} {2 3 1}} {a {2 1} {2 1} {1 2} {3 2 1}} {a {2 1} {2 1} {2 1} {1 2 3}} {a {2 1} {2 1} {2 1} {1 3 2}} {a {2 1} {2 1} {2 1} {3 2 1}} {a {2 1} {1 2} {2 1} {1 2 3}} {a {2 1} {1 2} {1 2} {3 2 1}} {a {2 1} {1 2} {1 2} {3 1 2}} {a {2 1} {1 2} {1 2} {1 2 3}} {a {2 1} {1 2} {2 1} {1 3 2}} {a {2 1} {1 2} {1 2} {1 3 2}} {a {2 1} {1 2} {1 2} {2 1 3}} {a {2 1} {1 2} {2 1} {3 1 2}} {a {2 1} {1 2} {2 1} {2 1 3}} {a {2 1} {1 2} {1 2} {2 3 1}} {a {2 1} {1 2} {2 1} {3 2 1}} {a {1 2} {2 1} {1 2} {2 3 1}} {a {1 2} {1 2} {1 2} {2 3 1}} {a {1 2} {1 2} {1 2} {3 1 2}} {a {1 2} {1 2} {2 1} {1 3 2}} {a {1 2} {1 2} {1 2} {1 3 2}} {a {1 2} {1 2} {2 1} {2 1 3}} {a {1 2} {1 2} {2 1} {1 2 3}} {a {1 2} {1 2} {2 1} {3 1 2}} {a {1 2} {1 2} {2 1} {3 2 1}} {a {1 2} {1 2} {1 2} {3 2 1}} {a {1 2} {1 2} {2 1} {2 3 1}} {a {1 2} {1 2} {1 2} {1 2 3}} {a {1 2} {1 2} {1 2} {2 1 3}} {a {1 2} {2 1} {2 1} {3 2 1}} {a {1 2} {2 1} {2 1} {1 3 2}} {a {1 2} {2 1} {2 1} {2 3 1}} {a {1 2} {2 1} {1 2} {3 2 1}} {a {1 2} {2 1} {1 2} {2 1 3}} {a {1 2} {2 1} {2 1} {2 1 3}} {a {1 2} {2 1} {1 2} {1 3 2}} {a {1 2} {2 1} {2 1} {1 2 3}} {a {1 2} {2 1} {2 1} {3 1 2}} {a {1 2} {2 1} {1 2} {1 2 3}} {a {1 2} {2 1} {1 2} {3 1 2}} {b {} {} {} {}} {b {1 2} {1 2} {2 1} {3 1 2}} {b {1 2} {2 1} {1 2} {3 2 1}} {b {1 2} {2 1} {2 1} {2 1 3}} {b {1 2} {2 1} {2 1} {2 3 1}} {b {1 2} {2 1} {2 1} {1 3 2}} {b {1 2} {2 1} {2 1} {3 2 1}} {b {1 2} {2 1} {1 2} {1 3 2}} {b {1 2} {2 1} {2 1} {3 1 2}} {b {1 2} {2 1} {1 2} {3 1 2}} {b {1 2} {2 1} {1 2} {2 1 3}} {b {1 2} {2 1} {1 2} {1 2 3}} {b {1 2} {2 1} {1 2} {2 3 1}} {b {1 2} {2 1} {2 1} {1 2 3}} {b {1 2} {1 2} {2 1} {1 2 3}} {b {1 2} {1 2} {2 1} {3 2 1}} {b {1 2} {1 2} {1 2} {1 3 2}} {b {1 2} {1 2} {1 2} {2 1 3}} {b {1 2} {1 2} {1 2} {1 2 3}} {b {1 2} {1 2} {2 1} {2 3 1}} {b {1 2} {1 2} {1 2} {3 2 1}} {b {1 2} {1 2} {1 2} {3 1 2}} {b {1 2} {1 2} {2 1} {2 1 3}} {b {1 2} {1 2} {1 2} {2 3 1}} {b {1 2} {1 2} {2 1} {1 3 2}} {b {2 1} {1 2} {1 2} {2 3 1}} {b {2 1} {2 1} {2 1} {3 2 1}} {b {2 1} {2 1} {1 2} {1 3 2}} {b {2 1} {2 1} {2 1} {3 1 2}} {b {2 1} {2 1} {1 2} {2 3 1}} {b {2 1} {2 1} {2 1} {2 1 3}} {b {2 1} {2 1} {1 2} {3 2 1}} {b {2 1} {2 1} {2 1} {1 2 3}} {b {2 1} {2 1} {2 1} {1 3 2}} {b {2 1} {2 1} {2 1} {2 3 1}} {b {2 1} {2 1} {1 2} {3 1 2}} {b {2 1} {2 1} {1 2} {2 1 3}} {b {2 1} {2 1} {1 2} {1 2 3}} {b {2 1} {1 2} {2 1} {1 2 3}} {b {2 1} {1 2} {1 2} {1 3 2}} {b {2 1} {1 2} {1 2} {2 1 3}} {b {2 1} {1 2} {2 1} {3 2 1}} {b {2 1} {1 2} {2 1} {1 3 2}} {b {2 1} {1 2} {1 2} {1 2 3}} {b {2 1} {1 2} {1 2} {3 2 1}} {b {2 1} {1 2} {1 2} {3 1 2}} {b {2 1} {1 2} {2 1} {2 1 3}} {b {2 1} {1 2} {2 1} {2 3 1}} {b {2 1} {1 2} {2 1} {3 1 2}}
+  data  length 18
+c 203 211 222
+c 203 212 221
+a 001 012 023
+a 002 013 021
+b 101 113 122
+b 102 113 121
+b 101 112 123
+c 202 211 223
+c 201 212 223
+c 201 213 222
+b 102 111 123
+c 202 213 221
+b 103 112 121
+a 001 013 022
+a 003 012 021
+a 003 011 022
+a 002 011 023
+b 103 111 122
 
-    data  length 96
-    a {2 1} {1 2} {2 1} {2 3 1}
-    b {1 2} {1 2} {2 1} {3 1 2}
-    b {2 1} {1 2} {1 2} {2 3 1}
-    b {2 1} {2 1} {2 1} {3 2 1}
-    b {2 1} {2 1} {1 2} {1 3 2}
-    b {2 1} {1 2} {2 1} {1 2 3}
-    a {1 2} {2 1} {1 2} {2 3 1}
-    b {2 1} {1 2} {1 2} {1 3 2}
-    a {2 1} {2 1} {2 1} {2 3 1}
-    a {2 1} {2 1} {1 2} {3 1 2}
-    b {1 2} {2 1} {1 2} {3 2 1}
-    a {1 2} {1 2} {1 2} {2 3 1}
-    b {1 2} {1 2} {2 1} {1 2 3}
-    b {2 1} {1 2} {1 2} {2 1 3}
-    a {1 2} {2 1} {2 1} {3 2 1}
-    b {2 1} {1 2} {2 1} {3 2 1}
-    b {2 1} {1 2} {2 1} {1 3 2}
-    b {2 1} {1 2} {1 2} {1 2 3}
-    b {1 2} {2 1} {2 1} {2 1 3}
-    b {1 2} {2 1} {2 1} {2 3 1}
-    a {2 1} {2 1} {2 1} {3 1 2}
-    b {2 1} {2 1} {2 1} {3 1 2}
-    a {1 2} {1 2} {1 2} {3 1 2}
-    a {2 1} {1 2} {2 1} {1 2 3}
-    b {2 1} {2 1} {1 2} {2 3 1}
-    b {1 2} {2 1} {2 1} {1 3 2}
-    a {2 1} {2 1} {2 1} {2 1 3}
-    a {1 2} {2 1} {2 1} {1 3 2}
-    b {2 1} {2 1} {2 1} {2 1 3}
-    a {1 2} {2 1} {2 1} {2 3 1}
-    b {1 2} {2 1} {2 1} {3 2 1}
-    a {1 2} {1 2} {2 1} {1 3 2}
-    a {1 2} {1 2} {1 2} {1 3 2}
-    a {2 1} {2 1} {1 2} {1 3 2}
-    b {2 1} {2 1} {1 2} {3 2 1}
-    b {2 1} {1 2} {1 2} {3 2 1}
-    a {1 2} {2 1} {1 2} {3 2 1}
-    a {1 2} {2 1} {1 2} {2 1 3}
-    b {1 2} {1 2} {2 1} {3 2 1}
-    b {2 1} {2 1} {2 1} {1 2 3}
-    b {1 2} {2 1} {1 2} {1 3 2}
-    a {1 2} {1 2} {2 1} {2 1 3}
-    b {2 1} {2 1} {2 1} {1 3 2}
-    a {2 1} {1 2} {1 2} {3 2 1}
-    a {2 1} {2 1} {1 2} {2 1 3}
-    a {1 2} {2 1} {2 1} {2 1 3}
-    b {1 2} {1 2} {1 2} {1 3 2}
-    b {2 1} {1 2} {1 2} {3 1 2}
-    a {2 1} {1 2} {1 2} {3 1 2}
-    b {2 1} {1 2} {2 1} {2 1 3}
-    b {1 2} {1 2} {1 2} {2 1 3}
-    a {1 2} {2 1} {1 2} {1 3 2}
-    b {1 2} {1 2} {1 2} {1 2 3}
-    a {2 1} {2 1} {1 2} {1 2 3}
-    b {1 2} {1 2} {2 1} {2 3 1}
-    b {1 2} {1 2} {1 2} {3 2 1}
-    a {2 1} {2 1} {1 2} {2 3 1}
-    a {1 2} {1 2} {2 1} {1 2 3}
-    b {1 2} {1 2} {1 2} {3 1 2}
-    b {1 2} {2 1} {2 1} {3 1 2}
-    a {2 1} {1 2} {1 2} {1 2 3}
-    b {1 2} {2 1} {1 2} {3 1 2}
-    b {2 1} {2 1} {2 1} {2 3 1}
-    a {2 1} {1 2} {2 1} {1 3 2}
-    b {1 2} {1 2} {2 1} {2 1 3}
-    a {2 1} {1 2} {1 2} {1 3 2}
-    b {2 1} {1 2} {2 1} {2 3 1}
-    b {1 2} {2 1} {1 2} {2 1 3}
-    a {2 1} {2 1} {1 2} {3 2 1}
-    a {2 1} {1 2} {1 2} {2 1 3}
-    a {1 2} {2 1} {2 1} {1 2 3}
-    b {1 2} {1 2} {1 2} {2 3 1}
-    a {2 1} {2 1} {2 1} {1 2 3}
-    a {2 1} {2 1} {2 1} {1 3 2}
-    a {2 1} {1 2} {2 1} {3 1 2}
-    a {1 2} {1 2} {2 1} {3 1 2}
-    a {2 1} {1 2} {2 1} {2 1 3}
-    a {1 2} {2 1} {2 1} {3 1 2}
-    b {1 2} {1 2} {2 1} {1 3 2}
-    b {1 2} {2 1} {1 2} {1 2 3}
-    a {1 2} {2 1} {1 2} {1 2 3}
-    a {1 2} {2 1} {1 2} {3 1 2}
-    a {1 2} {1 2} {2 1} {3 2 1}
-    a {1 2} {1 2} {1 2} {3 2 1}
-    a {1 2} {1 2} {2 1} {2 3 1}
-    b {2 1} {2 1} {1 2} {3 1 2}
-    b {1 2} {2 1} {1 2} {2 3 1}
-    a {2 1} {1 2} {1 2} {2 3 1}
-    a {1 2} {1 2} {1 2} {1 2 3}
-    b {2 1} {2 1} {1 2} {2 1 3}
-    b {1 2} {2 1} {2 1} {1 2 3}
-    b {2 1} {2 1} {1 2} {1 2 3}
-    a {1 2} {1 2} {1 2} {2 1 3}
-    b {2 1} {1 2} {2 1} {3 1 2}
-    a {2 1} {1 2} {2 1} {3 2 1}
-    a {2 1} {2 1} {2 1} {3 2 1}
 }
