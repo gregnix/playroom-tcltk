cdab2ca652a783f0c60b13472c6af7bc1c92d085
gregnix <77677781+gregnix@users.noreply.github.com>
2024-06-18 23:23:42 +0200

    Add files via upload

--- tablelist/tree/dict-tree-tablelist-load.tcl
+++ tablelist/tree/dict-tree-tablelist-load.tcl
@@ -1,5 +1,7 @@
 #! /usr/bin/env tclsh
 
+#20240618
+
 #dict - nested
 source [file join [file dirname [info script]] dict-tree-lib.tcl]
 
@@ -43,18 +45,58 @@ proc dataToTblTree {data} {
     }
     return $tree
 }
+proc dataToTblTreeAttr {data} {
+    set tree [dict create]
+    set counter 0
+    set pos 1
+    set pid 0
+    set pcounter 0
+    set part root
+    set subDict [dict create]
+    foreach item $data {
+        set length [llength $item]
+        incr counter
+        if {$length} {
+            incr counter -1
+        }
+        set plist [list]
+        set ppid $pid
+        for {set i 0} {$i < $length } {incr i} {
+            incr counter
+            set part [lindex $item $i]
+            lappend plist $part
+            if {![catch {getAttrValue tree $plist}]} {
+                set pid [dict get [dict get [lrange [getAttrValue tree $plist] 0 7]] pid]
+                set pcounter [dict get [dict get [lrange [getAttrValue tree $plist] 0 7]] counter]
+                set ppid [dict get [dict get [lrange [getAttrValue tree $plist] 0 7]] ppid]
+            } else {
 
-proc treeToTblLoad {tree colslist } {
+                if {!$i} {
+                    set ppid 0
+                } else {
+                    set ppid $pid
+                }
+                set pid $pos
+                incr pos
+                set pcounter $counter
+            }
+            addToTree tree $plist value "part $part pid $pid counter $pcounter ppid $ppid" 
+        }
+    }
+    return $tree
+}
+proc treeAttrToTblLoad {tree colslist } {
     set repeat [expr {[llength $colslist] -2}]
     set sortlist "0 increasing\n"
-    set liste [walkTreeList tree {}]
-
+    set liste [concat {*}[walkTree tree {} cmdListAttr]]
+    set liste [walkTree tree {} cmdListNode]
     set datalist [list [list root 0 {*}[lrepeat $repeat {}]]]
     set parentlist {-1}
-
-    foreach {k value}  $liste {
-        lappend datalist [list [lindex $value 1] [lindex $value 3] {*}[lrepeat $repeat {}]]
-        lappend parentlist [lindex $value 7]
+    foreach item $liste {
+        set k [lindex $item 0]
+        set value [dict get [lrange $item 1 end] value]
+        lappend datalist [list [dict get $value part] [dict get $value pid] {*}[lrepeat $repeat {}]]
+        lappend parentlist [dict get $value ppid]
     }
 
     append loadString $colslist\n
@@ -79,15 +121,20 @@ set data {
 puts \n
 #tablelist loadString
 set colslist [list id row one two]
-set tree [dataToTblTree $data]
-set loadstring [treeToTblLoad $tree $colslist]
+set tree [dataToTblTreeAttr $data]
+set loadstring [treeAttrToTblLoad $tree $colslist]
 puts $loadstring
 #end
 
 puts \n
-puts "walkTree tree {} printNode"
-walkTree tree {} printNode
+#puts "walkTree tree {} cmdPrintNode"
+#walkTree tree {} cmdPrintNode
 
+set tree [dataToTblTreeAttr $data]
+ puts \n
+#puts "walkTree tree {} cmdPrintNode"
+#walkTree tree {} cmdPrintNode   
+#puts [getAttrValue tree {a} ppid   ] 
 }
 
 
