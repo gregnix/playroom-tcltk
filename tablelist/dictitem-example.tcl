afe7570ee9958982a2c537d70df70c1bcd7aa4ae
gregnix <77677781+gregnix@users.noreply.github.com>
2024-07-25 22:30:59 +0200

    Update dictitem-example.tcl

--- tablelist/dictitem-example.tcl
+++ tablelist/dictitem-example.tcl
@@ -35,11 +35,14 @@ pack $tbl -expand yes -fill both
 set KeyList [$tbl getfullkeys 0 end]
 set IdList  [$tbl getkeys 0 end]
 
+puts "  \$tbl getkeys 0 : [$tbl getkeys 0]"
+puts "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
 # Sort the table by the third column ("Age") in increasing order
 $tbl sortbycolumn 2 -increasing
 
-# Output the first key after sorting
-puts [$tbl getkeys k0]
+puts "  #Output the first key after sorting"
+puts "  \$tbl getkeys 0 : [$tbl getkeys 0]"
+puts "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
 
 puts " foreach rowcget k"
 # Iterate over the keys using rowcget
@@ -67,6 +70,17 @@ puts " foreach get k"
 # Iterate over the keys using get
 foreach key $KeyList {
     set item [$tbl get $key]
+    set value [$tbl itemtodict $item]
+    puts $value
+    dict set data1 [string map {k ""} $key] $value
+}
+puts $data1
+
+unset data1
+puts " foreach get getkeys k"
+# Iterate over the keys using get
+foreach key $KeyList {
+    set item [$tbl get [$tbl getkeys $key]]
     set value [$tbl itemtodict $item]
     puts $value
     dict set data1 [string map {k ""} $key] $value
@@ -102,14 +116,17 @@ unset data1
 
 # Output:
 if {0} {
-
  data:
 
     0 {forename "John" surname "Doe" age "30"}
     1 {forename "Jane" surname "Smith" age "25"}
     2 {forename "Alice" surname "Johnson" age "40"}
 
-0
+  $tbl getkeys 0 : 0
+  $tbl getfullkeys 0 : k0
+  #Output the first key after sorting
+  $tbl getkeys 0 : 1
+  $tbl getfullkeys 0 : k1
  foreach rowcget k
 forename John surname Doe age 30
 forename Jane surname Smith age 25
@@ -125,6 +142,11 @@ forename John surname Doe age 30
 forename Jane surname Smith age 25
 forename Alice surname Johnson age 40
 0 {forename John surname Doe age 30} 1 {forename Jane surname Smith age 25} 2 {forename Alice surname Johnson age 40}
+ foreach get getkeys k
+forename Jane surname Smith age 25
+forename John surname Doe age 30
+forename Alice surname Johnson age 40
+0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
  foreach get
 forename Jane surname Smith age 25
 forename John surname Doe age 30
@@ -135,6 +157,5 @@ forename Jane surname Smith age 25"
 forename John surname Doe age 30"
 forename Alice surname Johnson age 40"
 0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
-
     
 }
