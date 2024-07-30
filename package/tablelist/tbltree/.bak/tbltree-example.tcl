dc9597e3e412322780bd8c0a9068fa9d36d73693
gregnix <gregor.ebbing@gmx.de>
2024-07-30 10:36:27 +0200

    organize

---
+++ package/tablelist/tbltree/.bak/tbltree-example.tcl
@@ -0,0 +1,56 @@
+#!/usr/bin/env tclsh
+
+package require Tk
+package require tablelist_tile
+set dirname [file dirname [info script]]
+source [file join $dirname tbltreedict.tcl]
+
+# Create the Tablelist widget with tree configuration
+proc createTbl {w} {
+   set frt [ttk::frame $w.frt]
+   set tbl [tablelist::tablelist $frt.tbl -columns {0 "Key" 0 "Value" 0 "Detail"} -height 20 -width 0 \
+    -stretch all -treecolumn 0]
+   $tbl columnconfigure 0 -name key
+   $tbl columnconfigure 1 -name value
+   set vsb [scrollbar $frt.vsb -orient vertical -command [list $tbl yview]]
+   set hsb [scrollbar $frt.hsb -orient horizontal -command [list $tbl xview]]
+   $tbl configure -yscroll [list $vsb set] -xscroll [list $hsb set]
+
+   tbl::init_moveMBind $tbl
+   tbl::init_moveKBind $tbl
+   pack $vsb -side right -fill y
+   pack $hsb -side bottom -fill x
+   pack $tbl -expand yes -fill both
+
+   pack $frt -expand yes -fill both
+   return $tbl
+}
+
+
+
+proc main {} {
+    set i 1
+    set data$i [dict create a$i [list b1$i c1 b1$i c1]]
+    set i 2
+    set data$i [dict create a$i [list b1$i c1 b2$i c1]]
+    set data [dict create]
+    dict set data a1 b1 $data1
+    dict set data a2 b1 $data1
+    dict set data a1 b1 $data2
+    dict set data a2 b1 $data2
+    dict set data a1 b2 $data1
+    dict set data a2 b2 $data1
+    dict set data a1 b2 $data2
+    dict set data a2 b2 $data2 
+  
+   ttk::frame .fr
+   pack .fr -side top -expand 1 -fill both
+
+   set tbl [createTbl  .fr]
+   tbl::dict2tbltree $tbl root $data
+   tbl::init_moveKBind $tbl
+   tbl::init_moveMBind $tbl
+   puts [tbl::tbltree2dict $tbl root]
+
+}
+main
