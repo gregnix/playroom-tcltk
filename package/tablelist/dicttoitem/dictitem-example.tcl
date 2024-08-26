43e8d9d13a75e0a0b0941a4ecc2aa629e7a04eb9
gregnix <gregor.ebbing@gmx.de>
2024-08-26 07:08:26 +0200

    tablelist lib tdbc sqlite3

--- package/tablelist/dicttoitem/dictitem-example.tcl
+++ package/tablelist/dicttoitem/dictitem-example.tcl
@@ -1,10 +1,27 @@
 package require Tcl 8.6
 package require tablelist_tile
+package require ctext
 
+# text window for information
+proc createText {w} {
+ set frt [ttk::frame $w.frtext]
+ set t [ctext $frt.t -setgrid true -wrap word -width 120 \
+    -yscrollcommand "$frt.vsb set" -xscrollcommand "$frt.hsb set"]
+ set vsb [scrollbar $frt.vsb -orient vertical -command "$t yview"]
+ set hsb [scrollbar $frt.hsb -orient horizontal -command "$t xview"]
+ pack $hsb -side bottom -fill x
+ pack $vsb -side right -fill y
+ pack $t -side left -fill both -expand true
+ pack $frt -expand yes -fill both
+ return $t
+}
 
-proc createTbl {} {
-    set w  ""
-    set frt [ttk::labelframe $w.frt ]
+proc textwins {w text} {
+    $w insert end "${text}\n"
+}
+
+proc createTbl {w} {
+    set frt [ttk::labelframe $w.frtbl ]
     # Create the Tablelist widget
     set tbl [tablelist::tablelist $frt.tbl -columns {
         0 "First Name" left
@@ -36,16 +53,19 @@ proc createTbl {} {
 
 }
 
-set tbl [createTbl]
-
+ttk::frame .frm
+set tbl [createTbl .frm]
+set text [createText .frm]
+pack .frm -expand 1 -fill both
 # Example data as a dictionary
 set data {
     0 {forename "John" surname "Doe" age "30"}
     1 {forename "Jane" surname "Smith" age "25"}
     2 {forename "Alice" surname "Johnson" age "40"}
 }
-puts " data:"
-puts $data
+
+textwins $text " data:"
+textwins $text $data
 
 
 
@@ -61,127 +81,81 @@ foreach {key personData} $data {
 set KeyList [$tbl getfullkeys 0 end]
 set IdList  [$tbl getkeys 0 end]
 
-puts "  \$tbl getkeys 0 : [$tbl getkeys 0]"
-puts "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
+textwins $text "  \$tbl getkeys 0 : [$tbl getkeys 0]"
+textwins $text "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
 # Sort the table by the third column ("Age") in increasing order
 $tbl sortbycolumn 2 -increasing
 
-puts "  #Output the first key after sorting"
-puts "  \$tbl getkeys 0 : [$tbl getkeys 0]"
-puts "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
+textwins $text "  #Output the first key after sorting"
+textwins $text "  \$tbl getkeys 0 : [$tbl getkeys 0]"
+textwins $text "  \$tbl getfullkeys 0 : [$tbl getfullkeys 0]"
 
-puts " foreach rowcget k"
+textwins $text " foreach rowcget k"
 # Iterate over the keys using rowcget
 foreach key $KeyList {
     set item [$tbl rowcget $key -text]
     set value [$tbl itemtodict $item]
-    puts $value
+    textwins $text $value
     dict set data1 [string map {k ""} $key] $value
 }
-puts $data1
+textwins $text $data1
 
 unset data1
-puts " foreach rowcget"
+textwins $text " foreach rowcget"
 # Iterate over the IDs using rowcget
 foreach key $IdList {
     set item [$tbl rowcget $key -text]
     set value [$tbl itemtodict $item]
-    puts $value
+    textwins $text $value
     dict set data1 $key $value
 }
-puts $data1
+textwins $text $data1
 
 unset data1
-puts " foreach get k"
+textwins $text " foreach get k"
 # Iterate over the keys using get
 foreach key $KeyList {
     set item [$tbl get $key]
     set value [$tbl itemtodict $item]
-    puts $value
+    textwins $text $value
     dict set data1 [string map {k ""} $key] $value
 }
-puts $data1
+textwins $text $data1
 
 unset data1
-puts " foreach get getkeys k"
+textwins $text " foreach get getkeys k"
 # Iterate over the keys using get
 foreach key $KeyList {
     set item [$tbl get [$tbl getkeys $key]]
     set value [$tbl itemtodict $item]
-    puts $value
+    textwins $text $value
     dict set data1 [string map {k ""} $key] $value
 }
 puts $data1
 
 unset data1
-puts " foreach get"
+textwins $text " foreach get"
 # Iterate over the IDs using get
 foreach key $IdList {
     set item [$tbl get $key ]
     set value [$tbl itemtodict $item]
-    puts $value
+    textwins $text $value
     dict set data1 $key $value
 }
-puts $data1
+textwins $text $data1
 
 unset data1
 
-puts "  for "
+textwins $text "  for "
 # Loop through the rows using a for loop
 set rowCount [$tbl size]
 for {set i 0} {$i < $rowCount} {incr i} {
     set itemData [$tbl get $i]
     set itemDict [$tbl itemtodict $itemData]
-    puts $itemDict"
+    textwins $text $itemDict"
     dict set data1 $i $itemDict
 }
-puts $data1
+textwins $text $data1
 
 unset data1
 
-
-# Output:
-if {0} {
- data:
-
-    0 {forename "John" surname "Doe" age "30"}
-    1 {forename "Jane" surname "Smith" age "25"}
-    2 {forename "Alice" surname "Johnson" age "40"}
-
-  $tbl getkeys 0 : 0
-  $tbl getfullkeys 0 : k0
-  #Output the first key after sorting
-  $tbl getkeys 0 : 1
-  $tbl getfullkeys 0 : k1
- foreach rowcget k
-forename John surname Doe age 30
-forename Jane surname Smith age 25
-forename Alice surname Johnson age 40
-0 {forename John surname Doe age 30} 1 {forename Jane surname Smith age 25} 2 {forename Alice surname Johnson age 40}
- foreach rowcget
-forename Jane surname Smith age 25
-forename John surname Doe age 30
-forename Alice surname Johnson age 40
-0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
- foreach get k
-forename John surname Doe age 30
-forename Jane surname Smith age 25
-forename Alice surname Johnson age 40
-0 {forename John surname Doe age 30} 1 {forename Jane surname Smith age 25} 2 {forename Alice surname Johnson age 40}
- foreach get getkeys k
-forename Jane surname Smith age 25
-forename John surname Doe age 30
-forename Alice surname Johnson age 40
-0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
- foreach get
-forename Jane surname Smith age 25
-forename John surname Doe age 30
-forename Alice surname Johnson age 40
-0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
-  for 
-forename Jane surname Smith age 25"
-    forename John surname Doe age 30"
-forename Alice surname Johnson age 40"
-    0 {forename Jane surname Smith age 25} 1 {forename John surname Doe age 30} 2 {forename Alice surname Johnson age 40}
-
-}
